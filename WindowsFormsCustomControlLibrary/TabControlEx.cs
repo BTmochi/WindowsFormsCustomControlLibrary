9162f72968e72849603342ad73786c35452a8560
BTmochi <botamochibtbtmochimochi@gmail.com>
2020-09-27 00:35:26 +0900

    初期コミット

---
+++ WindowsFormsCustomControlLibrary/TabControlEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsCustomControlLibrary
+{
+    public partial class TabControlEx : TabControl
+    {
+        public TabControlEx()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
